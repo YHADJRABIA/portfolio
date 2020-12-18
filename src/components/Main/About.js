25dc068961fe1e76b777cb50e2e424edd4b77c2d
YHADJRABIA <h_yacine@live.fr>
2020-12-18 17:39:21 +0100

    Updating content and adding photo animation

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -1,16 +1,19 @@
 import React from "react";
 import photo from "../img/photo.jpg"; // Personal picture
 import Flip from "react-reveal/Flip"; // Reveal effect
+import Roll from "react-reveal/Roll";
 const About = ({ i18n }) => {
   return (
     <>
       <section id="about">
-        <img
-          className="mugshot"
-          src={photo}
-          alt={i18n.t("myself")}
-          title={i18n.t("myself")}
-        ></img>
+        <Roll left>
+          <img
+            className="mugshot"
+            src={photo}
+            alt={i18n.t("myself")}
+            title={i18n.t("myself")}
+          ></img>
+        </Roll>
         <Flip top>
           <h2> {i18n.t("about.title")}</h2>
           <p>{i18n.t("about.content")}</p>
